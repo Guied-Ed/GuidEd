08657bf53d7aacbf85c747e2f4881e9b33499fad
bascom241 <135514268+bascom241@users.noreply.github.com>
2024-12-31 10:13:23 +0100

    Futher Email Configuration

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ import userRouter from './routes/userRouter';
 
 app.use(express.json());
 app.use('/api',userRouter);
-const port: number = parseInt(process.env.PORT || '3000' ,10);
+const port: number = parseInt(process.env.PORT || '3000',10);
 
 app.listen(port, (): void => {
     connectDB();
