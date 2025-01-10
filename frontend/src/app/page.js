4556d67c287f344d867dbd6e0fb521239fd8dae9
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-01-10 23:54:32 +0100

    Adding Frontend Folder of Next js

---
+++ frontend/src/app/page.js
@@ -0,0 +1,14 @@
+
+import NavBar from "@/components/NavBar";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <>
+
+
+      <div>This is the Home Page </div>
+      <Button variant="outline">Button</Button>
+    </>
+
+  );
+}
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex'>
+      <p>This  is the navbar Section </p>
+    </div>
+  )
+}
+
+export default NavBar
