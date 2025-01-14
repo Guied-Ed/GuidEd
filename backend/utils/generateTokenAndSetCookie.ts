1645f73298d73c0ffa0fa043538b9b996b356013
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-14 14:59:19 +0100

    chore: update backend configuration and add Prettier setup
    - Add `.gitignore` entries for log files and logs directory
    - Create `.prettierrc` file for code formatting configuration
    - Update `dbConnection.ts` to improve error handling and logging
    - Enhance `index.ts` to include network address detection and protocol selection
    - Add `format` script to `package.json` for running Prettier on all TypeScript files
    - other files too are formatted.

--- backend/utils/generateTokenAndSetCookie.ts
+++ backend/utils/generateTokenAndSetCookie.ts
@@ -1,15 +1,16 @@
-import jwt from 'jsonwebtoken'
-import  { Response } from 'express'
-
-
-
+import jwt from 'jsonwebtoken';
+import { Response } from 'express';
 
 const generateTokenAndSetToken = (res: Response, userId: string) => {
     if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET is not defined in the environment variables");
+        throw new Error(
+            'JWT_SECRET is not defined in the environment variables',
+        );
     }
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+    });
 
     res.cookie('token', token, {
         httpOnly: true,
